52a0f6c1b509b85e02f2b4e4683bee1334fe3955
Jesus Bejar <israelbejarii@gmail.com>
2025-03-13 10:40:32 -0500

    made array destructuring use case

---
+++ mini-demos/destructuring-a/scripts.ts
@@ -0,0 +1,12 @@
+// this
+const jArray = ['Jesus Christ', 'Jack Bauer', 'John Carter', 'Jack Reacher'];
+const [first, third] = jArray;
+console.log(first);
+
+// replaces this
+const jOne = jArray[0];
+const jTwo = jArray[1];
+const jThree = jArray[2];
+const jFour = jArray[3];
+console.log(jOne);
+
--- mini-demos/destructuring-o/scripts.ts
+++ mini-demos/destructuring-o/scripts.ts
@@ -1,15 +1,17 @@
 // change imminent (let)
 let a: number, b: number, c: number;
-// this 
-const obj = { a, b, c } = { a: 1, b: 2, c: 3 };
+// this
+const obj = ({ a, b, c } = { a: 1, b: 2, c: 3 });
 // replaces this
-a = obj.a; b = obj.b; c = obj.c;
+a = obj.a;
+b = obj.b;
+c = obj.c;
 console.log(a);
 console.log(obj);
 
 // no change imminent (const)
 const { x, y, z } = { x: 1, y: 2, z: 3 };
-console.log(z)
+console.log(z);
 // how to access obj without var name?
 
 const objTwo = {
@@ -20,4 +22,5 @@ const objTwo = {
 const { movieOne } = objTwo;
 const { name: movieOneName, releaseDate } = movieOne;
 
-document.querySelector("#display")!.innerHTML = `<p> ${movieOneName} ${releaseDate} </p>`
+document.querySelector('#display')!.innerHTML =
+  `<p> ${movieOneName} ${releaseDate} </p>`;
