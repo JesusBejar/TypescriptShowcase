a48f1a08bc39e81c0dc6dfa649d7e9b42f686871
Jesus Bejar <israelbejarii@gmail.com>
2025-03-13 10:24:33 -0500

    made object destructuring use case

--- mini-demos/destructuring-o/scripts.ts
+++ mini-demos/destructuring-o/scripts.ts
@@ -1,8 +1,23 @@
-const obj = { a: 1, b: 2, c: 3 };
-const { a, b, c } = obj;
+// change imminent (let)
+let a: number, b: number, c: number;
+// this 
+const obj = { a, b, c } = { a: 1, b: 2, c: 3 };
+// replaces this
+a = obj.a; b = obj.b; c = obj.c;
 console.log(a);
+console.log(obj);
+
+// no change imminent (const)
+const { x, y, z } = { x: 1, y: 2, z: 3 };
+console.log(z)
+// how to access obj without var name?
 
 const objTwo = {
   movieOne: { name: 'Jack Reacher', releaseDate: '2012' },
   movieTwo: { name: 'Mission Impossible III', releaseDate: '2006' },
 };
+
+const { movieOne } = objTwo;
+const { name: movieOneName, releaseDate } = movieOne;
+
+document.querySelector("#display")!.innerHTML = `<p> ${movieOneName} ${releaseDate} </p>`
