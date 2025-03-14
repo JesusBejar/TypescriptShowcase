4f868c0a10bac43d58711f22552c2a93cfe71e3b
Jesus Bejar <israelbejarii@gmail.com>
2025-03-14 09:36:44 -0500

    added comments, defined type and obj destructuring use case

--- pixel-pets-manager/src/main.ts
+++ pixel-pets-manager/src/main.ts
@@ -1,24 +1,26 @@
-import './style.css';
-import typescriptLogo from './typescript.svg';
-import viteLogo from '/vite.svg';
-import { setupCounter } from './counter.ts';
+type Pet = {
+  name: string | null;
+  hunger: number;
+  stats: [number, number]; // energy, mood levels
+  owner?: { name: string };
+}
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
+const pet: Pet = {
+  name: null,
+  hunger: 30,
+  stats: [80, 60],
+  owner: { name: "Israel" },
+};
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
+// object destructuring, display how hungry the pet is along with name
+// const {props from obj}: {assign to new vars} = objName
+const { hunger, name }: { hunger: number; name: string | null } = pet;
+console.log(`${name ?? "Furlais"} is ${hunger}% hungry`);
+
+// array destructuring, display energy and mood of pet
+
+// spread, update pet hunger from 30 to 20
+
+// rest, make list of pets and grab info
+
+// optional chaining + null coalescing, pull pet owner's name 
